25130f4375f7c1fff411f63052713ae5f74cf30e
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2023-12-24 12:39:40 +0300

    Update train.py

--- train/train.py
+++ train/train.py
@@ -17,9 +17,9 @@ from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
 from sklearn.model_selection import KFold, cross_val_score, StratifiedKFold, cross_val_predict ,train_test_split
 import warnings
-from utils import *
+from train.utils import *
 warnings.filterwarnings("ignore")
-df = pd.read_csv("bank.csv")
+df = pd.read_csv(os.path.join(os.getcwd(),"train/bank.csv"))
 X, X_test, y, y_test = split_data(df)
 X = add_features(X)
 X_test = add_features(X_test)
